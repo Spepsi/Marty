78fe10b25d7fba28670b4f843415ad1d40a0d60f
ldocao <ldocao@quantmetry.com>
2017-06-10 21:53:34 +0200

    fix: FUCK YEAH ca marche

--- Marty/domain/image_similarity/perceptual_hashing/phash.py
+++ Marty/domain/image_similarity/perceptual_hashing/phash.py
@@ -1,50 +1,55 @@
 import os
-import ipdb
-
-import cv2
+import glob
 import pandas as pd
+import cv2
 import functools
 from PIL import Image
 import imagehash
-
-import matplotlib.pyplot as plt
-plt.style.use('classic')
 
 
 from Marty.domain.crop.image import Image as ImageCropper
 
 
-@functools.lru_cache(maxsize=None, typed=False)
+#@functools.lru_cache(maxsize=None, typed=False)
 def get_image_hash(path):
-    return imagehash.average_hash(Image.open(path))
+    return imagehash.phash(Image.open(path), hash_size=16)
+
+
+
+
+
+
+
 
     
 class PHash(object):
+    DATABASE = "/Users/ldocao/Google Drive/Documents/Professionnel/2015 10 26 Quantmetry/Externe/01 Missions/2017 06 10 Hackathon Marty/Marty/data/pict"
+    
     def __init__(self, image):
-        self.crop(image)
-        self.image = "temporary.jpg"
-        self.hash = get_image_hash(self.image)
-
-
-    @staticmethod
-    def crop(image):
-        cropped = ImageCropper(image).crop()
-        try:
-            os.remove("temporary.jpg")
-        except FileNotFoundError:
-            pass
-        cv2.imwrite("temporary.jpg", cropped)
-
+        """
+        Parameters
+        ----------
+        image: str
+            Path to file
+        """
+        self.image = image
+        self.hash = get_image_hash(image)
 
         
-    def closest_from(self, image_list):
-        distances = self.get_distances(image_list)
+    def closest_from_database(self, n=1):
+        """
+        Parameters
+        ----------
+        n: int
+            Top N closest images found in database (default:1)
+        """
+        distances = self.get_distances()
         distances.sort_values("distance", ascending=True, inplace=True)
-        print(distances)
-        return distances.index[0]
+        return distances.index[0:n+1]
     
         
-    def get_distances(self, image_list):
+    def get_distances(self):
+        image_list = self._find_images()
         primary_key = [i.split("/")[-1:][0] for i in image_list]
         distances = pd.DataFrame(columns=["distance"], index=primary_key)
         for i in image_list:
@@ -54,4 +59,11 @@ class PHash(object):
 
         return distances
             
+    @classmethod
+    def _find_images(cls):
+        directory = cls.DATABASE
+        regexp = "*.jpg"
+        lookup = os.path.join(directory, regexp)
+        image_list = glob.glob(lookup)
+        return image_list
         
