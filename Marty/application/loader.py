a8ae1a88b8450482ce7ed20cffeca0733074d02b
Florian Gardin <fgardin@quantmetry.com>
2017-06-10 18:05:58 +0200

    add second step recognition first try

--- Marty/application/loader.py
+++ Marty/application/loader.py
@@ -1,6 +1,40 @@
 from Marty.utils.config.config import  Config
-from Marty.infrastructure.loader.loader import JSONLabels
+from Marty.infrastructure.loader.loader import JSONLabels, JSONUrls, JSONSafeSearch, LabelsComparator
+from Marty.infrastructure.google_api import GoogleVisionApi
+import os
 config = Config(file='../../config.ini')
-json_ = JSONLabels(config)
+
+
+labels = JSONLabels(config)
+url = JSONUrls(config)
+safe_search = JSONSafeSearch(config)
+
 
 from pdb import set_trace; set_trace()
+image_source = "https://2.bp.blogspot.com/-C6qwZKrvxB0/UMjVf5cN_7I/AAAAAAAAIjE/aNuiUQc2sDY/s1600/chimney1.jpg"
+vision = GoogleVisionApi(config, image_source=image_source)
+
+labels = vision.get_labels()
+url = vision.get_full_matching_urls()
+# Do we have a full match ?
+# No ... get partial match
+partials = vision.get_partial_matching_urls()
+# Suppose not, then we do a query on full matching url [0]
+vision_second = GoogleVisionApi(config, image_source=partials[0])
+# get labels
+labels_second = vision_second.get_labels()
+# Check we have a match in database
+labels_comparator = LabelsComparator(labels_second)
+database = os.listdir(config['DATA']['JSON'])
+database = [int(i.split('.')[0]) for i in database]
+for idx in database:
+    labels_to_compare = JSONLabels(config, idx)
+    res =  labels_comparator(labels_to_compare)
+    if res==True:
+        print(" It is idx : "+idx)
+# Comparer avec toute la base
+# Création de la base de données de mots ?
+
+# Dans le cas ou google api ne renvoie pas exactement les url
+# On relance une requête de mot sur l'url donné et on compare les résultats avec notre base
+
--- Marty/infrastructure/loader/loader.py
+++ Marty/infrastructure/loader/loader.py
@@ -1,6 +1,24 @@
 import json
 import os
 
+
+
+class UrlComparator:
+    def __init__(self, labels_to_compare):
+        self.labels_to_compare = labels_to_compare
+    def __call__(self, labels):
+        labs = set([description for description, score in labels])
+        labs2 = set([description for description, score in self.labels_to_compare])
+        return labs==labs2
+
+class LabelsComparator:
+    def __init__(self, labels_to_compare):
+        self.labels_to_compare = labels_to_compare
+    def __call__(self, labels):
+        labs = set([description for description, score in labels])
+        labs2 = set([description for description, score in self.labels_to_compare])
+        return labs==labs2
+
 class JSONLabels:
     def __init__(self, config):
         self.location = config['DATA']['JSON']
@@ -9,6 +27,24 @@ class JSONLabels:
             text = json.load(f_)
         text = text['responses'][0]['labelAnnotations']
         text = [(i['description'], i['score']) for i in text]
-
         return text
 
+
+class JSONUrls:
+    def __init__(self, config):
+        self.location = config['DATA']['JSON']
+    def load(self, idx):
+        with open(os.path.join(self.location+str(idx)+'.json'), 'r') as f_:
+            text = json.load(f_)
+        text = text['responses'][0]['webDetection']['partialMatchingImages']
+        text = [i['url'] for i in text]
+        return text
+
+class JSONSafeSearch:
+    def __init__(self, config):
+        self.location = config['DATA']['JSON']
+    def load(self, idx):
+        with open(os.path.join(self.location+str(idx)+'.json'), 'r') as f_:
+            text = json.load(f_)
+        text = text['responses'][0]['safeSearchAnnotation']
+        return text
