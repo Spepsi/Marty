ed3dd1601bf7211e0f179eabc694079225dff388
Nicolas Gibaud <ngibaud@quantmetry.com>
2017-06-10 17:17:07 +0200

    NOISE

--- Marty/domain/matching_google.py
+++ Marty/domain/matching_google.py
@@ -1,6 +1,7 @@
 from Marty.infrastructure.google_api import GoogleVisionApi
 from Marty.utils.config.config import Config
 
+import os
 import pandas as pd
 
 config = Config()
@@ -23,3 +24,27 @@ catalog_file = "/Users/nicolas/Projects/Marty/data/catalog/catalog.csv"
 catalog = pd.read_csv(catalog_file, sep=";", encoding="cp1250")
 
 class GoogleApiMatcher():
+    def __init__(self, config):
+        self.config = config
+        data_dir = config["DATA"]["MAIN"]
+        catalog_file = os.path.join(data_dir, "catalog", "catalog.csv")
+        self.catalog = pd.read_csv(catalog_file, sep=";", encoding="cp1250")
+
+    def find_perfect_match(self, image_source):
+        api = GoogleVisionApi(self.config, image_source)
+        partial_matching_urls_with_score = api.get_partial_matching_urls()
+        partial_matching_urls = [partial_url[0] for partial_url in partial_matching_urls_with_score]
+
+        index = None
+        for url in partial_matching_urls:
+            catalog_filtered = self.catalog[self.catalog["URL"] == url]
+            #if len(catalog_filtered) == 0:
+                #print("No matching found for {}".format(url))
+            if len(catalog_filtered) == 1:
+                index = catalog_filtered.index
+                print("yeah!")
+                break
+            #else:
+                #print("More than one match found")
+
+        return index
