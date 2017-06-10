b49890661ea235f928725934dc39ae13357210ca
Nicolas Gibaud <ngibaud@quantmetry.com>
2017-06-10 10:57:21 +0200

    REFACTOR: Create repository structure

---
+++ Marty/utils/config/config.py
@@ -0,0 +1,16 @@
+import os
+import configparser
+
+class Config:
+    def __new__(self, file=None):
+        config = configparser.ConfigParser()
+
+        # we use the config.ini file in the same repository as default
+        if file is None:
+            file = "config.ini"
+        else:
+            if not os.path.exists(file):
+                raise IOError("File {} does not exist".format(file))
+
+        config.read(file)
+        return config
